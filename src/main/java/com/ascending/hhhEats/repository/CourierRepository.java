a17fd975f9b609ac597f29b76dd028067f8205c1
Tianjie Shen <tianjie.shen@rhsmith.umd.edu>
2019-04-17 22:38:31 -0400

    Courier interface

--- src/main/java/com/ascending/hhhEats/repository/CourierRepository.java
+++ src/main/java/com/ascending/hhhEats/repository/CourierRepository.java
@@ -1,4 +1,12 @@
 package com.ascending.hhhEats.repository;
 
-public interface CourierRepository {
+import com.ascending.hhhEats.domain.Courier;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CourierRepository extends CrudRepository<Courier, Long> {
+    List<Courier> findDistinctByGenderEqualsAndVehicle(String gender, String vehicle);
+    List<Courier> findAllById(Long id);
+    List<Courier> findCourierByName(String name);
 }
