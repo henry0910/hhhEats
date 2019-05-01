5996abea171fbe6e22dc07e0001eb98ae82d1e07
Tianjie Shen <tianjie.shen@rhsmith.umd.edu>
2019-04-30 20:28:49 -0400

    postman test pass

--- src/main/java/com/ascending/hhhEats/api/V1/MenuController.java
+++ src/main/java/com/ascending/hhhEats/api/V1/MenuController.java
@@ -5,14 +5,16 @@ import com.ascending.hhhEats.service.MenuService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@Controller
+@ResponseBody
+@RequestMapping(value = {"/api/menus", "/api/menu"}, produces = MediaType.APPLICATION_JSON_VALUE)
 public class MenuController {
     @Autowired
     private MenuService menuService;
--- src/main/java/com/ascending/hhhEats/api/V1/OrderController.java
+++ src/main/java/com/ascending/hhhEats/api/V1/OrderController.java
@@ -6,14 +6,16 @@ import com.ascending.hhhEats.service.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@Controller
+@ResponseBody
+@RequestMapping(value = {"/api/orders", "/api/user"}, produces = MediaType.APPLICATION_JSON_VALUE)
 public class OrderController {
     @Autowired
     private OrderService orderService;
