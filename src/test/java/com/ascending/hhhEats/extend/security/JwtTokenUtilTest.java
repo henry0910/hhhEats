ed2ffb383ee0e19577d619811f86a46cb68f5068
Tianjie Shen <tianjie.shen@rhsmith.umd.edu>
2019-05-24 01:24:33 -0400

    jwt token test

--- src/test/java/com/ascending/hhhEats/extend/security/JwtTokenUtilTest.java
+++ src/test/java/com/ascending/hhhEats/extend/security/JwtTokenUtilTest.java
@@ -1,16 +1,39 @@
 package com.ascending.hhhEats.extend.security;
 
 import com.ascending.hhhEats.config.AppConfig;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import javax.transaction.Transactional;
 
 @WebAppConfiguration
 @ContextConfiguration(classes = {AppConfig.class })
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles("Unit")
 public class JwtTokenUtilTest {
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
 
+    @Test
+    @Transactional
+    public void generateTokenTest() {
+
+    }
+
+    @Test
+    @Transactional
+    public void getUsernameFromTokenTest() {
+
+    }
+
+    @Test
+    @Transactional
+    public void isTokenExpiredTest() {
+
+    }
 }
